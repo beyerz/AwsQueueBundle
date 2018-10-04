54d0f98f4cad64d3d05924c762f887fb724d3a8c
Marco Rieger <m.rieger@mittwald.de>
2018-10-04 14:18:00 +0200

    fulfill interface

--- Fabric/GearmanFabric.php
+++ Fabric/GearmanFabric.php
@@ -9,15 +9,37 @@
 namespace Beyerz\AWSQueueBundle\Fabric;
 
 
+use Beyerz\AWSQueueBundle\Consumer\ConsumerService;
+use Doctrine\Common\Collections\ArrayCollection;
+
 class GearmanFabric extends AbstractFabric
 {
-
     /**
      * Fabric should ensure that all notification channels and respective queues exist and subscribers are defined
      * @return mixed
      */
-    public function setup()
+    public function setup(string $channel, ArrayCollection $subscribers)
     {
         // TODO: Implement setup() method.
+    }
+
+    public function setupNotificationServices(string $channel)
+    {
+        // TODO: Implement setupNotificationServices() method.
+    }
+
+    public function setupQueueServices(string $channel, ArrayCollection $subscribers)
+    {
+        // TODO: Implement setupQueueServices() method.
+    }
+
+    public function publish(string $message, string $channel, ArrayCollection $subscribers)
+    {
+        // TODO: Implement publish() method.
+    }
+
+    public function consume(ConsumerService $consumer, int $messageCount): int
+    {
+        // TODO: Implement consume() method.
     }
 }
